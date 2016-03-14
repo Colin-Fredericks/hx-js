8031f2e4a7c54d8055aa16271511c67ca4225354
Colin-Fredericks <colin.fredericks@gmail.com>
2016-03-14 13:41:52 -0400

    Initial commit

---
+++ hxGlobalOptions.js
@@ -0,0 +1,10 @@
+/********************************************/
+// Global options for hx.js
+// If you haven't changed anything here, it's showing you the default options.
+// Change things here to set them course-wide.
+// To reset to default, just remove them or reupload the original
+// version of this file from the GitHub repository.
+/********************************************/
+
+hxShowUTCClock = false;
+
