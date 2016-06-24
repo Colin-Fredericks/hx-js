7c2e40daa3348e3d9b1317006391da0054a45587
Colin-Fredericks <colin.fredericks@gmail.com>
2016-06-24 11:23:27 -0400

    Stop UTC clock from appearing multiple times.

--- hx.js
+++ hx.js
@@ -295,9 +295,13 @@ var HXGlobalJS = (function(hxLocalOptions, HXPUPTimer) {
         // Set hxLocalOptions.showUTCClock = true to use.
         /**************************************/
         if(hxOptions.showUTCClock){
-            var hxClockFrame = '<li style="float:right;"><iframe src="https://freesecure.timeanddate.com/clock/i53t5o51/fc5e5e5e/tct/pct/ftb/ts1/ta1" title="UTC Clock" frameborder="0" width="100" height="16" style="padding-left: 11px; padding-top: 11px;"></iframe></div>';
-            var hxClockSpot = $('.course-tabs');
-            hxClockSpot.append(hxClockFrame);
+            // Only add the clock if it isn't already there.
+            if($('#hx-utcclock').length == 0){
+                var hxClockFrame = '<li style="float:right;"><iframe id="hx-utcclock" Title="UTC clock" src="https://freesecure.timeanddate.com/clock/i53t5o51/fc5e5e5e/tct/pct/ftb/ts1/ta1" title="UTC Clock" frameborder="0" width="100" height="16" style="padding-left: 11px; padding-top: 11px;"></iframe></div>';
+                var hxClockSpot = $('.course-tabs');
+                hxClockSpot.append(hxClockFrame);
+                logThatThing('UTC clock added');
+            }
         }
 
 
