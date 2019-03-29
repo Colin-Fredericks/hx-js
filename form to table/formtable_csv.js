b8a2b740f3c1a42ab3f059bcbe09ba90218bd4d3
Colin-Fredericks <colin.fredericks@gmail.com>
2019-03-28 17:12:44 -0700

    Trimming

--- form to table/formtable_csv.js
+++ form to table/formtable_csv.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
       if (typeof Papa !== 'undefined') {
         clearInterval(waitForPapa);
         loadNewData(sourceDataFile);
-        // setUpControls();
       }
     }, 250);
   } else {
@@ -57,61 +56,6 @@ function loadNewData(filename) {
     }
   });
 }
-
-/*
-// Sort table columns
-function setUpControls(datafiles) {
-  console.log('Setting up data table controls.');
-  // If there's only one data file, skip this.
-  if (datafiles.length === 1) {
-    return false;
-  }
-
-  // If we've already got a dropdown, skip this.
-  let hasDropDown = $(document).find('#map-data-dropdown').length > 0;
-  if (hasDropDown) {
-    return;
-  }
-
-  let wrapper = $(document).find('.mapwrapper');
-  let form = $('<form id="map-data-dropdown"/>');
-  let fieldset = $('<fieldset/>');
-
-  let datalabel = $('<label for="mapdatapicker">Select map data: </label>');
-
-  let sel = $('<select name="mapdatapicker" id="mapdatapicker"></select>');
-
-  for (let i = 0; i < datafiles.length; i++) {
-    let opt = $('<option>');
-    opt.text(datafiles[i].name);
-    sel.append(opt);
-  }
-
-  wrapper
-    .append(form)
-    .append(fieldset)
-    .append(datalabel)
-    .append(sel);
-
-  // Add dropdown listener.
-  sel.on('change', function() {
-    let newName = this.value;
-    let newData = datafiles.find(x => x.name === newName).filename;
-
-    // Recolor the map.
-    loadNewMapData(newData);
-
-    // Change the download link and (possibly) map title.
-    let downloadLink = $(document).find('#map_data_download');
-    let mapTitle = $(document).find('#map_title');
-    downloadLink.text(newName);
-    mapTitle.text(newName);
-    downloadLink.prop('href', newData);
-  });
-
-  return true;
-}
-*/
 
 function shortenText(text, n) {
   if (text.indexOf('https://courses.edx.org/courses') !== -1) {
