6edc766dea6226167c23a7bbd716e64b6f207165
Colin-Fredericks <colin.fredericks@gmail.com>
2020-02-19 16:49:50 -0500

    Lots of little fixes

--- HXHelper Files/HXEditor.js
+++ HXHelper Files/HXEditor.js
@@ -16,40 +16,42 @@ var HXEditor = function(useBackpack, toolbarOptions) {
   });
 
   // Insert a loading indicator.
-  let editbox = $('<div class="editorloading"> Loading...</div>');
+  let editbox = $('<div class="editorbox"> Loading...</div>');
   let spinner = $('<span class="fa fa-spinner fa-pulse"></span>');
   editbox.prepend(spinner);
-  editors.append(loading);
+  editors.append(editbox);
 
   // Wait for summernote to load.
   // It's in an external javascript file loaded by hx-js.
   var timercount = 0;
-  var timedelay = 0.25; // seconds
-  var loadLoop = setTimeout(function() {
+  var timedelay = 250; // miliseconds
+  var loadLoop = setInterval(function() {
     timercount += timedelay;
 
     // If it doesn't load after 7 seconds,
     // kill the indicator and inform the learner.
-    if (timercount > 7) {
+    if (timercount > 7000) {
       editbox.empty();
       editbox.append(
         '<p>Editor did not load. Reload the page if you want to try again.</p>'
       );
-      clearTimeout(loadLoop);
+      clearInterval(loadLoop);
     }
 
-    if (typeof summernote !== 'undefined') {
+    if (typeof $.summernote !== 'undefined') {
       // If it loads...
       // Remove the loading indicator.
       editbox.empty();
       // Insert the div for summernote to hook onto.
-      editors.append('<div class="summernote"></div>');
+      let summer = $('<div class="summernote"></div>');
+      editors.append(summer);
       // Activate summernote.
-      $('.summernote').summernote({
+      summer.summernote({
         toolbar: toolbarOptions
       });
+      clearInterval(loadLoop);
     }
-  }, timedelay * 1000);
+  }, timedelay);
 
   // If we're not using the backpack, show a warning notice.
   if (!useBackpack) {
@@ -70,39 +72,55 @@ var HXEditor = function(useBackpack, toolbarOptions) {
   let saveindicator = $('<span/>');
 
   let loadbutton = $('<button>Load</button>');
-  savebutton.addClass('loadnote');
+  loadbutton.addClass('loadnote');
+
   editors.prepend(saveindicator);
   editors.prepend(savebutton);
   editors.prepend(loadbutton);
 
   // Add listeners for save/load buttons.
   // Append the editor's id to their save slot.
-  $('#savenote').on('click tap', function() {
-    let save_slot = this.parent().attr('data-saveslot');
+  $('.savenote').on('click tap', function() {
     let markupStr = $('.summernote').summernote('code');
 
     // If we try to save while the backpack is recovering from auto-saving,
     // don't bother. Just show the success indicator.
     if (true) {
-      hxSetData('summernote_' + save_slot, markupStr);
+      hxSetData('summernote_' + getSaveSlot($(this)), markupStr);
       console.log(markupStr);
     }
     // TODO: Indicate success for saving.
   });
-  $('#loadnote').on('click tap', function() {
-    let parentID = this.parent().attr('data-saveslot');
-    $('.summernote').summernote('code', hxGetData('summernote_' + save_slot));
+  $('.loadnote').on('click tap', function() {
+    $('.summernote').summernote(
+      'code',
+      hxGetData('summernote_' + getSaveSlot($(this)))
+    );
   });
 
   // Set up loop to auto-save once/minute
-  let autoSave = setTimeout(function() {
+  let autoSave = setInterval(function() {
     var to_save = {};
     editors.each(function(i, e) {
-      let save_slot = e.attr('data-saveslot');
       let markupStr = $('#summernote').summernote('code');
       to_save['summernote_' + saveslot] = markupStr;
     });
     hxSetData(to_save);
     console.log('auto-saved');
   }, 60000);
+
+  // The save slot is the value in data-saveslot attribute, or '' if blank.
+  // Pass in a JQuery object that's inside the editor's parent,
+  // such as the save or load buttons.
+  function getSaveSlot(t) {
+    try {
+      if (typeof t.attr('data-saveslot') === 'undefined') {
+        return '';
+      } else {
+        return t.attr('data-saveslot');
+      }
+    } catch (err) {
+      return '';
+    }
+  }
 };
