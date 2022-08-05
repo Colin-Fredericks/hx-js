3545c76e723787e8d750f30e407a573dd841052f
Colin-Fredericks <Colin-Fredericks@users.noreply.github.com>
2022-08-05 16:02:37 -0400

    Erase all uploads

---
+++ public_html/erase_all_uploads.js
@@ -0,0 +1,102 @@
+/****************************
+ * Course Content Eraser for edX
+ * Runs via bookmarklet.
+ * Written by Colin Fredericks at HarvardX
+ * MIT licensed
+ ****************************/
+
+$(document).ready(function () {
+  console.log('erase_all_uploads called');
+
+  showWarningDialog();
+
+  function showWarningDialog() {
+    // Do they really want to delete every single thing in Files & Uploads?
+
+    makeModal();
+    $('#modal-1').dialog({
+      title: 'Double-Checking Your Intent',
+      modal: true,
+      width: '50%',
+      buttons: [
+        {
+          text: 'I changed my mind.',
+          click: function () {
+            $('#modal-1').dialog('destroy');
+          },
+        },
+        {
+          text: 'DESTROY.',
+          click: function () {
+            destroyEverything();
+            console.log('erasing everything');
+          },
+        },
+      ],
+      open: function (event, ui) {
+        $('.ui-dialog-titlebar-close').hide();
+        $('.ui-dialog').focus();
+        $('.ui-widget-overlay').on('click', function () {
+          $('#modal-1').dialog('close');
+        });
+      },
+    });
+    console.log('testing');
+  }
+
+  function destroyEverything() {
+    // Try to delete a file every .5 seconds.
+
+    let timer = setInterval(function () {
+      let delete_buttons = $("button[data-identifier='asset-delete-button']");
+      if (delete_buttons.length > 0) {
+        delete_buttons[0].click();
+        // look for a visible confirmation dialog every .2 seconds
+        let inner_timer = setInterval(function () {
+          let confirm_button = $(
+            "button[data-identifier='asset-confirm-delete-button']:visible"
+          );
+          if (confirm_button.length > 0) {
+            clearInterval(inner_timer);
+            confirm_button[0].click();
+          }
+        }, 200);
+      } else {
+        clearInterval(timer);
+      }
+    }, 500);
+  }
+
+  function makeModal() {
+    if ($('#modal-1').length > 0) {
+      console.log('modal already exists');
+      return;
+    }
+
+    let d = $('<div>');
+    d.attr('id', 'modal-1');
+
+    let content = $('<main>');
+    content.attr('id', 'modal-1-content');
+
+    let explanation = $('<p>');
+    explanation.text(
+      'Are you really sure you want to delete EVERYTHING in Files and Uploads?'
+    );
+
+    let user_type = $('#member-lists-selector option:selected').text();
+    let details = $('<p style="font-size: small;"></p>');
+    details.text(
+      'Maybe you want to take a backup of the course first so you can save the SRT files? ' +
+        'Maybe you want to search for a specific file type first and run this again to just delete those? ' +
+        'No "undo" button here FYI.'
+    );
+
+    content.append(explanation);
+    content.append(details);
+
+    d.append(content);
+    $('body').append(d);
+    console.log('modal made');
+  }
+});
